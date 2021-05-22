92e562d6a48a4cf9e7a55f1688195ec66425954a
Andrew Marsh <21085039+asmarsh@users.noreply.github.com>
2021-05-22 12:11:57 -0400

    Implement readers

--- ConnectionReader.cs
+++ ConnectionReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace PolymorphicConfigurationReader
     {
         public string Read(string key)
         {
-            throw new NotImplementedException();
+            return ConfigurationManager.ConnectionStrings[key].ConnectionString;
         }
     }
 }
